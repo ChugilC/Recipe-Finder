38a5d28b20522e746c04a106f230d11c378ad76e
Chugil.C <chugilrc13@gmail.com>
2020-09-06 15:25:49 +0530

    Initial Commit

--- script.js
+++ script.js
@@ -46,6 +46,44 @@ function searchMeal(e) {
 
 function getMealById(mealID) {
     fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
+      .then(res => res.json())
+      .then(data => {
+          const meal = data.meals[0];
+
+          addMealToDOM(meal);
+      });
+}
+
+
+function addMealToDOM(meal) {
+    const ingredients = [];
+
+    for(let i=1; i<=20; i++) {
+        if(meal[`strIngredient${i}`]) {
+            ingredients.push(`${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`);
+        } else {
+            break;
+        }
+    }
+
+    single_mealEl.innerHTML = `
+      <div class="single-meal">
+        <h1>${meal.strMeal}</h1>
+        <img src="${meal.strMealThumb}" />
+        <div class="single-meal-info">
+           ${meal.strCategory ? `<p>${meal.strCategory}</p>` : ''}
+           ${meal.strArea ? `<p>${meal.strArea}</p>` : ''}
+        </div>
+
+        <div class="main">
+          <p>${meal.strInstructions}</p>
+          <h2>Ingredients</h2>
+          <ul>
+            ${ingredients.map(ing => `<li>${ing}</li>`).join('')}
+          </ul>
+        </div>
+      </div>
+    `;
 }
 
 
